a73e2f2dabd097d5b01eccc8e49d3d03816bea58
SheikhRokon <mdrokon.u545@gmail.com>
2024-11-19 14:58:07 +0600

    urls add

---
+++ student_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from student_app.views import *
+
+urlpatterns = [
+    path('', student_list, name='student_list'),
+    path('create/', create_student, name='create_student'),
+    path('view/', view_student, name='view_student'),
+    path('update/<str:student_id>/', update_student, name='update_student'),
+    path('delete/<str:student_id>/', delete_student, name='delete_student'),
+]
